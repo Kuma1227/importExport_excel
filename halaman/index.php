21c36d71ce4ee80d6cb7a0592a7b28615380e1d7
Saiful <kriegswar27@gmail.com>
2023-07-07 16:48:42 +0700

    day 1, atur halaman

---
+++ halaman/index.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Dashboard - SB Admin</title>
+        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
+        <link href="../resource/css/styles.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="sb-nav-fixed">
+        <?=
+            require 'bagian/navbar.php';
+        ?>
+        <?=
+            require 'bagian/sideNavbar.php';
+        ?>
+
+        <?=
+            require 'bagian/body.php';
+        ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="../resource/js/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="../resource/assets/demo/chart-area-demo.js"></script>
+        <script src="../resource/assets/demo/chart-bar-demo.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+        <script src="../resource/js/datatables-simple-demo.js"></script>
+    </body>
+</html>
